69c26f2664c7a0b3b898a7c82c76121ddf0e1de4
Efosa Asemota <efosa.asemota@morehouse.edu>
2020-05-14 20:52:59 -0400

    Is now formatting

--- read-write.py
+++ read-write.py
@@ -12,10 +12,11 @@ def writeOuput(y):
     z = open("test.html", "w")
     z.write("<!DOCTYPE html>")
     z.write("\n\t<html>")
-    z.write("\n\t\t<p>")
+    z.write("\n\t\t<code>")
     for lines in y:
         z.write("\n\t\t\t%s" %lines)
+        z.write("<br>")
         
-    z.write("\n\t\t</p>")
+    z.write("\n\t\t</code>")
     z.write("\n\t</html>")
 readFile()
