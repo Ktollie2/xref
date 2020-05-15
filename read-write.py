f338d552747a453036f4452697ff9d8ddc440d0f
Kevin Tolliver <kevintolliver@Kevins-MacBook-Pro-7.local>
2020-05-14 19:57:53 -0700

    Picks up comments and italiscizes them

--- read-write.py
+++ read-write.py
@@ -15,19 +15,26 @@ def writeOuput(y):
     z = open("test.html", "w")
     z.write("<!DOCTYPE html>")
     z.write("\n\t<html>")
-    z.write("\n\t\t<code>")
+    z.write("\n\t\t<p>")
     for lines in y:
+        in_comment = False
         for word in lines.split():
             is_keyword = False
-            for i in range(len(keywords)):
-                if(word == keywords[i]):
-                    z.write("<b>%s</b> " %word)
-                    is_keyword = True
-                elif(i == (len(keywords) - 1) and is_keyword == False):
-                    z.write(" %s " %word)
+            if ((in_comment == True) and ("//" in word) == False):
+                z.write("<i>%s</i> " %word)
+            elif ("/" in word):
+                in_comment = True
+                z.write("<i>%s</i> " %word)
+            else:
+                for i in range(len(keywords)):
+                    if(word == keywords[i]):
+                        z.write("<b>%s</b> " %word)
+                        is_keyword = True
+                    elif(i == (len(keywords) - 1) and is_keyword == False):
+                        z.write(" %s " %word)
         ##z.write("\n\t\t\t%s" %lines)
         z.write("<br>")
         
-    z.write("\n\t\t</code>")
+    z.write("\n\t\t</p>")
     z.write("\n\t</html>")
 readFile()
