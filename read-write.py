af3535694a4a3c2df17e1a31f63b79a682ea2381
Kevin Tolliver <kevintolliver@Kevins-MacBook-Pro-7.local>
2020-05-14 20:46:32 -0700

    Added ability to run terminal command on class file

--- read-write.py
+++ read-write.py
@@ -1,15 +1,24 @@
 from subprocess import *
+import os
 
 
 
 def readFile():
-    with open("AsemotaTurtle.java", "r") as x:
+    with open("AsemotaDice.java", "r") as x:
         writeOuput(x)
 
 
-
+def classFiles():
+    includes_classfile = input("do you have any class file Y/N?:")
+    if includes_classfile == "Y" or includes_classfile == "y":
+        class_file = input("Please insert path to class file")
+        os.system("javap -verbose -private " + class_file)
+    else:
+        return 0
+    
 def writeOuput(y):
 
+    ## List of every key word as listed by oracle
     keywords = ["abstract", "assert", "boolean", "break", "byte","case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", 'native', "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while"]
     is_keyword = False
     z = open("test.html", "w")
@@ -19,6 +28,7 @@ def writeOuput(y):
     for lines in y:
         in_comment = False
         for word in lines.split():
+            ## Checks if line contains comment and italiscizes if it does
             is_keyword = False
             if ((in_comment == True) and ("//" in word) == False):
                 z.write("<i>%s</i> " %word)
@@ -26,6 +36,7 @@ def writeOuput(y):
                 in_comment = True
                 z.write("<i>%s</i> " %word)
             else:
+                ## Cross checks each keyword and bold faces if keyword is found
                 for i in range(len(keywords)):
                     if(word == keywords[i]):
                         z.write("<b>%s</b> " %word)
@@ -37,4 +48,5 @@ def writeOuput(y):
         
     z.write("\n\t\t</p>")
     z.write("\n\t</html>")
+classFiles()
 readFile()
