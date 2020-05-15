5741df53b580c5978638c75641488ac3de5136b8
Kevin Tolliver <kevintolliver@Kevins-MacBook-Pro-7.local>
2020-05-14 19:34:12 -0700

    Implemented keyword check

--- read-write.py
+++ read-write.py
@@ -9,16 +9,22 @@ def readFile():
 
 
 def writeOuput(y):
+
+    keywords = ["abstract", "assert", "boolean", "break", "byte","case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", 'native', "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while"]
+    is_keyword = False
     z = open("test.html", "w")
     z.write("<!DOCTYPE html>")
     z.write("\n\t<html>")
     z.write("\n\t\t<code>")
     for lines in y:
         for word in lines.split():
-            if(word == "int"):
-                z.write("<b>%s</b> " %word)
-            else:
-                z.write(" %s " %word)
+            is_keyword = False
+            for i in range(len(keywords)):
+                if(word == keywords[i]):
+                    z.write("<b>%s</b> " %word)
+                    is_keyword = True
+                elif(i == (len(keywords) - 1) and is_keyword == False):
+                    z.write(" %s " %word)
         ##z.write("\n\t\t\t%s" %lines)
         z.write("<br>")
         
